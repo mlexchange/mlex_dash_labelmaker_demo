b2e9d39b52ec35219dfe9ee630b3b2a5ee2c4ca3
Tanny Chavez Esparza <tanchavez1@gmail.com>
2022-07-18 18:21:11 -0700

    fixed error when changing tabs with no data

--- front/src/helper_utils.py
+++ front/src/helper_utils.py
@@ -242,9 +242,10 @@ def get_trained_models_list(user, datapath, tab):
     model_list = requests.get(f'http://job-service:8080/api/v0/jobs?&user={user}&mlex_app={tab}').json()
     trained_models = [{'label': 'Default', 'value': 'data'+filename}]
     for model in model_list:
-        if model['job_kwargs']['kwargs']['dataset']==datapath[0]['file_path'] and \
-                model['job_kwargs']['kwargs']['job_type'].split(' ')[0]=='prediction_model':
-            if os.path.exists(model['job_kwargs']['cmd'].split(' ')[4]+filename):  # check if the file exists
-                trained_models.append({'label': model['job_kwargs']['kwargs']['job_type'],
-                                       'value': model['job_kwargs']['cmd'].split(' ')[4]+filename})
+        if datapath:
+            if model['job_kwargs']['kwargs']['dataset']==datapath[0]['file_path'] and \
+                    model['job_kwargs']['kwargs']['job_type'].split(' ')[0]=='prediction_model':
+                if os.path.exists(model['job_kwargs']['cmd'].split(' ')[4]+filename):  # check if the file exists
+                    trained_models.append({'label': model['job_kwargs']['kwargs']['job_type'],
+                                           'value': model['job_kwargs']['cmd'].split(' ')[4]+filename})
     return trained_models
