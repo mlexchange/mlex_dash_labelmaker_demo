ee4b858a2824147d87449b80f4ecb0b7fef261e8
taxe10 <tanchavez@lbl.gov>
2022-09-29 21:10:16 +0000

    added comments as placeholders for changes in api

--- front/src/labelmaker.py
+++ front/src/labelmaker.py
@@ -1062,6 +1062,12 @@ def save_labels_disk(button_save_disk_n_clicks, button_save_splash_n_clicks, clo
         if len(labels_name_data)>0:
             if 'button-save-splash.n_clicks' in changed_id:
                 response = save_to_splash(labels_name_data)
+                params1 = {'key': 'datapath'}
+                payload = [{'file_path': str(root), 'file_type': 'dir'}]
+                # {'key': 'splash'}
+                # {'tagging_event': ''}
+                # {'uris': ['filename1', 'filename2']} -- or {'uids': ['uid1', 'uid2']}
+                # resp = requests.post("http://labelmaker-api:8005/api/v0/export/datapath", params=params1, json=payload)
             else:
                 # create root directory
                 root = pathlib.Path(DOCKER_DATA / 'labelmaker_outputs' /str(uuid.uuid4()))
