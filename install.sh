197571ad9012218f17caac84f3e5ea525534a693
zhuowenzhao <zwenzhao11@gmail.com>
2023-04-03 12:31:24 -0700

    updated install

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ docker-compose -f docker-compose-models.yml pull
 docker tag mlexchange1/unsupervised-classifier:1.0 mlexchange1/unsupervised-classifier:latest
 docker tag mlexchange1/tensorflow-neural-networks:1.0 mlexchange1/tensorflow-neural-networks:latest
 
-docker-compose -f docker-compose-master-local.yml up -d
+docker-compose -f docker-compose-master-local.yml up --build -d
